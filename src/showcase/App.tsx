4801e8e2c5bed1c2526f33769f6a91ac5b5e64cc
letrananhvu <vule5726@gmail.com>
2022-12-15 23:41:01 +0200

    Update App.tsx

--- src/showcase/App.tsx
+++ src/showcase/App.tsx
@@ -1,20 +1,16 @@
-import './App.css';
-import calendarIcon from './assets/logo3.svg';
-import CalendarMonthExample from '@/showcase/components/CalendarMonthExample';
+import './App.css'
+import CalendarMonthExample from '@/showcase/components/CalendarMonthExample'
 
 function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={calendarIcon} className="app-logo" alt="Calendar lib logo" />
-        </a>
         <h1 className="h1">Calendar Lib Show Case</h1>
       </div>
       <hr />
       <CalendarMonthExample />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
