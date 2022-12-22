9585da95cf279644e79e383075d3821919ae8e44
tramtran0497 <tramtran0497@gmail.com>
2022-12-22 02:00:02 +0200

    Refactoring coding

--- src/lib/helpers/timeToHeightPx.ts
+++ src/lib/helpers/timeToHeightPx.ts
@@ -1,13 +1,16 @@
+
+
 /**
  * Convert a given hour as an argument and return number of pixel of the height value
- * @param timeToHeightPx
- *  Default an hour: 47px
+ * @param duration duration of time
+ * @param pixelPerHour number of pixel per hour
+ * @returns 
  */
-
- export const timeToHeightPx =  (hour: number): number| never => {
-    let numberOfPx = 0
-    if(hour > 0) {
-        return numberOfPx = hour * 47;
+ export const durationToHeightPx =  (duration: number, pixelPerHour: number): number => {
+    if(duration <= 0) {
+        throw new Error ("The duration argument must be greater than 0!")
     }
-    throw new Error ("The hour argument must be greater than 0!")
+
+    return  duration * pixelPerHour;
+
 }
--- src/lib/helpers/timeToTopPx.ts
+++ src/lib/helpers/timeToTopPx.ts
@@ -1,15 +1,18 @@
 
 /**
  * Convert the given hour and return number of pixel of the top value
- * @param timeToTopPx
- *  Default an hour: 47px
+ * @param hour 
+ * @param pixelPerHour number of pixel per hour
+ * @returns 
  */
-
-export const timeToTopPx =  (hour: number): number|never => {
-    let numberOfPx = 0
-    if(hour > 0) {
-        if(hour === 1) return numberOfPx
-        else return numberOfPx = hour * 47
+export const timeToTopPx =  (hour: number, pixelPerHour: number): number => {
+    if(hour <= 0) {
+        throw new Error ("The hour argument must be greater than 0!") 
     }
-    throw new Error ("The hour argument must be greater than 0!") 
+
+    if(hour === 1) {
+        return 0
+    } 
+    
+    return hour * pixelPerHour
 }
