c14f859e32dadc8330db44dbb7d1e071b89f5b92
Le Tran Anh Vu <vule5726@gmail.com>
2023-01-04 14:12:57 +0200

    Add mutual style for modal components

--- src/layout/components/Modals/Modal.tsx
+++ src/layout/components/Modals/Modal.tsx
@@ -6,6 +6,7 @@ import type { OperationalModalProps } from './OperationalModal'
 import OperationalModal from './OperationalModal'
 import { useContext } from 'react'
 import { ModalContext } from '@/layout/components/contexts/ModalContextProvider'
+import styled from 'styled-components'
 
 export enum ModalTypes {
   Notification,
@@ -37,19 +38,36 @@ export type ModalItem = NotificationModalItem | OperationalModalItem | ConfirmMo
 function Modal() {
   const { modals } = useContext(ModalContext)
 
-  return (
-    <div>
-      {modals.map((modal) => {
-        if (modal.type === ModalTypes.Notification) {
-          return <NotificationModal key={modal.id} {...modal.props} />
-        } else if (modal.type === ModalTypes.Confirmation) {
-          return <ConfirmModal key={modal.id} {...modal.props} />
-        } else {
-          return <OperationalModal key={modal.id} {...modal.props} />
-        }
-      })}
-    </div>
-  )
+  const hasAny = modals.length && modals.find((m) => m.props.isShowed)
+
+  if (!hasAny) {
+    return <></>
+  } else {
+    return (
+      <ModalWrapper>
+        {modals.map((modal) => {
+          if (modal.type === ModalTypes.Notification) {
+            return <NotificationModal key={modal.id} {...modal.props} />
+          } else if (modal.type === ModalTypes.Confirmation) {
+            return <ConfirmModal key={modal.id} {...modal.props} />
+          } else {
+            return <OperationalModal key={modal.id} {...modal.props} />
+          }
+        })}
+      </ModalWrapper>
+    )
+  }
 }
 
 export default Modal
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  z-index: 999;
+  justify-content: center;
+  background: #8c8c8c3d;
+`
--- src/layout/components/Modals/OperationalModal.tsx
+++ src/layout/components/Modals/OperationalModal.tsx
@@ -26,14 +26,12 @@ function OperationalModal({
       borderRadius={8}
       topOffsetOfModal={40}
       headerHeight={44}>
-      <div className="modal">
-        <div className="header">
-          <button onClick={onCancel}>{cancelText}</button>
-          <span>{title}</span>
-          <button onClick={onExecute}>{executeText}</button>
-        </div>
-        <div className="content">{component()}</div>
+      <div className="header">
+        <button onClick={onCancel}>{cancelText}</button>
+        <span>{title}</span>
+        <button onClick={onExecute}>{executeText}</button>
       </div>
+      <div className="content">{component()}</div>
     </OperationalModalWrapper>
   )
 }
@@ -49,57 +47,47 @@ type ModalWrapperProps = {
 
 const OperationalModalWrapper = styled.div<ModalWrapperProps>`
   display: ${({ isShowed }) => (isShowed ? 'flex' : 'none')};
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100vw;
-  z-index: 999;
-  justify-content: center;
-  background: #8c8c8c3d;
+  box-shadow: 4px 4px 8px ${({ theme }) => theme.black};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  max-height: calc(100% - ${({ topOffsetOfModal }) => topOffsetOfModal}px);
+  transform: translate(-50%, -50%);
+  width: 100%;
+  background: ${({ theme }) => theme.modalBackground};
+  border-radius: ${({ borderRadius }) => borderRadius}px;
+  padding-top: ${({ headerHeight }) => headerHeight}px;
 
-  .modal {
-    box-shadow: 4px 4px 8px ${({ theme }) => theme.black};
+  .header {
+    border-radius: ${({ borderRadius }) => borderRadius}px ${({ borderRadius }) => borderRadius}px 0
+      0;
+
+    top: 0;
+    left: 0;
     position: absolute;
-    top: 50%;
-    left: 50%;
-    max-height: calc(100% - ${({ topOffsetOfModal }) => topOffsetOfModal}px);
-    transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
     width: 100%;
     background: ${({ theme }) => theme.modalBackground};
-    border-radius: ${({ borderRadius }) => borderRadius}px;
-    padding-top: ${({ headerHeight }) => headerHeight}px;
+    padding: 10px 20px;
+    height: ${({ headerHeight }) => headerHeight}px;
+    box-shadow: 1px 0px 2px ${({ theme }) => theme.black};
+  }
+  .content {
+    padding: 0 20px;
+    overflow: scroll;
+    max-height: 550px;
+  }
 
-    .header {
-      border-radius: ${({ borderRadius }) => borderRadius}px ${({ borderRadius }) => borderRadius}px
-        0 0;
-
-      top: 0;
-      left: 0;
-      position: absolute;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      flex-direction: row;
-      width: 100%;
-      background: ${({ theme }) => theme.modalBackground};
-      padding: 10px 20px;
-      height: ${({ headerHeight }) => headerHeight}px;
-    }
+  @media ${devices.mobileL} {
     .content {
-      padding: 0 20px;
-      overflow: scroll;
-      max-height: 550px;
+      max-height: 600px;
     }
+  }
 
-    @media ${devices.mobileL} {
-      .content {
-        max-height: 600px;
-      }
-    }
-
-    @media ${devices.tablet} {
-      max-width: 500px;
-    }
+  @media ${devices.tablet} {
+    max-width: 500px;
   }
 `
