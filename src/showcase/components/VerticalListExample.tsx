6b981a731e379ac5f3e1939d39177b25115382ba
Le Tran Anh Vu <vule5726@gmail.com>
2023-01-05 17:42:40 +0200

    Add vertialList

---
+++ src/showcase/components/VerticalListExample.tsx
@@ -0,0 +1,27 @@
+import { VerticalItem, VerticalList } from '@/layout/components/VertialList/VerticalList'
+import { rangeFromOne } from '@/lib/array/range'
+import styled from "styled-components";
+
+function createItems() {
+  return rangeFromOne(100).map((i) => ({ text: 'this is item no: ' + i }))
+}
+
+function VerticalListExample() {
+  return (
+    <Wrapper>
+      <VerticalList style={{ maxHeight: '400px', maxWidth: '400px', width: '100%' }}>
+        {createItems().map((item) => (
+          <VerticalItem key={item.text}>{item.text}</VerticalItem>
+        ))}
+      </VerticalList>
+    </Wrapper>
+  )
+}
+
+export default VerticalListExample
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`
