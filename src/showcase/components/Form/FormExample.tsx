b6df5b856aad85520d18510a44de472812095bce
Le Tran Anh Vu <vule5726@gmail.com>
2023-01-08 14:14:31 +0200

    Add checkbox field and group fields

--- src/showcase/components/Form/FormExample.tsx
+++ src/showcase/components/Form/FormExample.tsx
@@ -4,6 +4,8 @@ import type { OptionTypeOfSelectField } from '@/layout/components/Form/SelectFie
 import SelectField from '@/layout/components/Form/SelectField'
 import { VerticalItem, VerticalList } from '@/layout/components/VerticalList'
 import { rangeFromOne } from '@/lib/array/range'
+import type { CheckboxValue } from '@/layout/components/Form/CheckboxField'
+import CheckboxGroup from '@/layout/components/Form/CheckboxGroup'
 
 function createOptions(n: number): OptionTypeOfSelectField[] {
   return rangeFromOne(n).map(
@@ -11,9 +13,38 @@ function createOptions(n: number): OptionTypeOfSelectField[] {
   )
 }
 
+const dayCheckboxValues: CheckboxValue[] = [
+  {
+    value: 'mon',
+    label: 'Monday',
+    checked: false
+  },
+  {
+    value: 'tue',
+    label: 'Tuesday',
+    checked: false
+  },
+  {
+    value: 'wed',
+    label: 'Wednesday',
+    checked: false
+  },
+  {
+    value: 'thu',
+    label: 'Thursday',
+    checked: false
+  },
+  {
+    value: 'fri',
+    label: 'Friday',
+    checked: false
+  }
+]
+
 function FormExample() {
   const listRef = useRef<HTMLUListElement>(null)
   const [selectValue, setSelectValue] = useState('dog')
+  const [checkboxValues, setCheckboxValues] = useState<CheckboxValue[]>(dayCheckboxValues)
   const getScrollableParent = () => {
     return listRef.current
   }
@@ -26,6 +57,14 @@ function FormExample() {
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     console.log('Selected value:', event.target.value)
     setSelectValue(event.target.value)
+  }
+
+  const handleCheckboxChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckboxValues(
+      checkboxValues.map((c) =>
+        c.value === event.target.value ? { ...c, checked: event.target.checked } : c
+      )
+    )
   }
 
   return (
@@ -45,6 +84,11 @@ function FormExample() {
             />
           </VerticalItem>
         </VerticalList>
+        <CheckboxGroup
+          label="Active days"
+          values={checkboxValues}
+          onChange={handleCheckboxChanged}
+        />
       </form>
     </div>
   )
